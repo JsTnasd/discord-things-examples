735d8bd59aa7035e14bf6de47a81d348fa9ca3a9
JsTn <97077253+JsTnasd@users.noreply.github.com>
2022-06-17 11:03:04 -0600

    Create memes.js

---
+++ examples/memes.js
@@ -0,0 +1,20 @@
+const { Memes } = require('discord-things') 
+//At the moment the memes will be in english
+
+await Memes({
+    message: message, //Object
+        boton_uno: { //First button customization
+          etiqueta: 'Remove', //Label
+          emoji: '🛑', //Emoji (Optional)
+          estilo: 'DANGER' //Style
+        },
+        boton_dos: { 
+          etiqueta: 'Next',
+          emoji: '▶️',
+          estilo: 'DANGER'
+        },
+        tiempo: '300000', //Time for disabled
+        autor: 'Only <@[usuario]> can use!', //Only the author
+        color: 'GREEN', //Embed color
+        remover: 'You remove the button!' //Remove button action
+       })
