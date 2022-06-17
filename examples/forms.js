8a2861ed6875fe0437923a4f1aecb2fbefcbb4df
JsTn <97077253+JsTnasd@users.noreply.github.com>
2022-06-17 11:02:06 -0600

    Create forms.js

---
+++ examples/forms.js
@@ -0,0 +1,12 @@
+const { Forms } = require('discord-things')
+
+await Forms({ 
+    message: message, //Object
+        embed: {
+         titulo: 'Form', //Title of embed
+         color: 'ORANGE', //Color of embed
+         pie: 'Footer Form' //Footer of embed
+        },
+        tiempo: '300000', //Time for disabled
+        preguntas: ['Country?', 'Age?'] //Array of the questions
+    })
